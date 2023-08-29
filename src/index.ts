b02489a06a2d99a51d653f2980fdd19873d1a091
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-08-29 07:25:41 -0400

    remove logging that failed to log what I wanted

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ dotenv.config();
 import { BaseShip } from "./models/Ship";
 import { prisma } from "./db/prismaClient";
 import { addShiptoDb } from "./db/addShipToDb";
+import { fleet } from "./fleet/ships";
 
 async function getShipsFromDb() {
   const allShips = await prisma.ship.findMany();
   console.log(allShips);
+  return allShips;
 }
 
 const flagshipFrigate = new BaseShip("SPACEBOATS-1");
@@ -16,13 +18,21 @@ const miningShip = new BaseShip("SPACEBOATS-3");
 
 const ships = ["SPACEBOATS-1", "SPACEBOATS-2", "SPACEBOATS-3"];
 
-// ships.forEach(async (ship) => {
-//   try {
-//     await addShiptoDb(ship);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+// uhhhh assumes that the table is empty?
+async function addStarterShipsToDb() {
+  const newShips = await fleet.getMyShips();
 
+  const shipSymbols = newShips.data.map((ship) => ship.symbol);
+
+  shipSymbols.forEach(async (ship) => {
+    try {
+      await addShiptoDb(ship);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+}
+
+// addStarterShipsToDb();
 getShipsFromDb();
 // flagshipFrigate.getStatus().then((res) => console.log(res));
