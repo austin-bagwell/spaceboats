a3ca740c468e370910d843d4db086228bdc41fd8
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-09 15:04:06 -0400

    add console log

--- src/db/getShipsFromDb.ts
+++ src/db/getShipsFromDb.ts
@@ -2,6 +2,8 @@ import { prisma } from "./prismaClient";
 
 async function getShipsFromDb() {
   const allShips = await prisma.ship.findMany();
+
+  console.log(`all me lovely ships: `);
   console.log(allShips);
   return allShips;
 }
