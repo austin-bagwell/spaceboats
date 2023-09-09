64052c90b918a20b0380a06c576a61b8a5054fe3
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-09 11:21:02 -0400

    cleaning some fluff

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import { BaseShip } from "./models/Ship";
-import { prisma } from "./db/prismaClient";
-import { addShiptoDb } from "./db/addShipToDb";
-import { fleet } from "./fleet/ships";
-
-async function getShipsFromDb() {
-  const allShips = await prisma.ship.findMany();
-  console.log(allShips);
-  return allShips;
-}
+import { myShips } from "./fleet/ships";
+import { getShipsFromDb } from "./db/getShipsFromDb";
 
 const flagshipFrigate = new BaseShip("SPACEBOATS-1");
 const surveryShip = new BaseShip("SPACEBOATS-2");
@@ -18,21 +11,6 @@ const miningShip = new BaseShip("SPACEBOATS-3");
 
 const ships = ["SPACEBOATS-1", "SPACEBOATS-2", "SPACEBOATS-3"];
 
-// uhhhh assumes that the table is empty?
-async function addStarterShipsToDb() {
-  const newShips = await fleet.getMyShips();
-
-  const shipSymbols = newShips.data.map((ship) => ship.symbol);
-
-  shipSymbols.forEach(async (ship) => {
-    try {
-      await addShiptoDb(ship);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-}
-
-// addStarterShipsToDb();
+myShips();
 getShipsFromDb();
 // flagshipFrigate.getStatus().then((res) => console.log(res));
