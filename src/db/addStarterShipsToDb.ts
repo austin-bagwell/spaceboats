51d3156ecd62796348289ad97fcfc055f364ab75
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-24 20:45:37 -0400

    use ShipPropsDB as type, temporarily

--- src/db/addStarterShipsToDb.ts
+++ src/db/addStarterShipsToDb.ts
@@ -1,12 +1,42 @@
 import { fleet } from "../fleet/ships";
 import { addShiptoDb } from "./addShipToDb";
 
+// export interface ShipPropsDB {
+//   shipSymbol: string;
+//   systemSymbol: string;
+//   waypointSymbol: string;
+//   navStatus: string;
+//   fuelCurrent: number;
+//   fuelCapacity: number;
+//   cargoCapacity: number;
+//   cargoOnboard: number;
+// }
+
 async function addStarterShipsToDb() {
   const newShips = await fleet.getMyShips();
 
-  const shipSymbols = newShips.data.map((ship) => ship.symbol);
+  // const shipSymbols = newShips.data.map((ship) => ship.symbol);
+  const ships = newShips.data.map((ship) => {
+    const {
+      symbol: shipSymbol,
+      nav: { systemSymbol, waypointSymbol, status: navStatus },
+      fuel: { capacity: fuelCapacity, current: fuelCurrent },
+      cargo: { capacity: cargoCapacity, units: cargoOnboard },
+    } = ship;
 
-  shipSymbols.forEach(async (ship) => {
+    return {
+      shipSymbol,
+      systemSymbol,
+      waypointSymbol,
+      navStatus,
+      fuelCapacity,
+      fuelCurrent,
+      cargoCapacity,
+      cargoOnboard,
+    };
+  });
+
+  ships.forEach(async (ship) => {
     try {
       await addShiptoDb(ship);
     } catch (err) {
