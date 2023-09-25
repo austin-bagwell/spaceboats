3f46db6cc98789943b5902146552313b875a31a5
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-09-24 20:54:49 -0400

    clean comments

--- src/db/addStarterShipsToDb.ts
+++ src/db/addStarterShipsToDb.ts
@@ -1,21 +1,9 @@
 import { fleet } from "../fleet/ships";
 import { addShiptoDb } from "./addShipToDb";
 
-// export interface ShipPropsDB {
-//   shipSymbol: string;
-//   systemSymbol: string;
-//   waypointSymbol: string;
-//   navStatus: string;
-//   fuelCurrent: number;
-//   fuelCapacity: number;
-//   cargoCapacity: number;
-//   cargoOnboard: number;
-// }
-
 async function addStarterShipsToDb() {
   const newShips = await fleet.getMyShips();
 
-  // const shipSymbols = newShips.data.map((ship) => ship.symbol);
   const ships = newShips.data.map((ship) => {
     const {
       symbol: shipSymbol,
