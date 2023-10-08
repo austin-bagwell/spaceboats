4384444000d0e31b7c2f761f68e1daa52685379d
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-07 21:08:31 -0400

    add veeeerrrry basic CLI interface

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,46 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-
-import { BaseShip } from "./models/Ship";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { GetMyShips200Response, Ship } from "@spacejunk/airlock";
 import { myShips } from "./fleet/ships";
-import { getShipsFromDb } from "./db/getShipsFromDb";
-import { getMyAgent } from "./agent/agent";
-import { addStarterShipsToDb } from "./db/addStarterShipsToDb";
 
-const flagshipFrigate = new BaseShip("SPACEBOATS-1");
-const surveryShip = new BaseShip("SPACEBOATS-2");
-const miningShip = new BaseShip("SPACEBOATS-3");
+const rl = readline.createInterface({ input, output });
 
-const ships = ["SPACEBOATS-1", "SPACEBOATS-2", "SPACEBOATS-3"];
+let game = true;
+async function startGame() {
+  while (game) {
+    const input = await rl.question(
+      "SPACEBOATS\nEnter a command, help, or quit: "
+    );
 
+    if (input.toLowerCase() === "quit" || input.toLowerCase() === "q") {
+      console.log(`Thanks for playing spaceboats`);
+      rl.close();
+      game = false;
+    }
+
+    if (input.toLowerCase() === "help" || input.toLowerCase() === "h") {
+      console.log(`no help yet, trying quitting instead`);
+    }
+
+    if (input.toLowerCase() === "fleet" || input.toLowerCase() === "f") {
+      console.log("fleet goes here");
+      const response = await myShips();
+      console.log(response.data);
+      //   ships.data.forEach((ship: Ship) => console.log(ship.symbol));
+      //   console.log(ships);
+
+      //   ships.forEach((ship: Ship) => {
+      //     console.log(ship.symbol);
+      //   });
+      //   const names = ships.map((ship: Ship) => ship.symbol);
+      //   console.log(ships);
+    }
+  }
+}
+
+startGame();
 // getMyAgent()
 //   .then((res) => console.log(res))
 //   .catch((err) => console.log(err));
@@ -21,5 +49,5 @@ const ships = ["SPACEBOATS-1", "SPACEBOATS-2", "SPACEBOATS-3"];
 //   console.log(res);
 // });
 // addStarterShipsToDb();
-getShipsFromDb();
+// getShipsFromDb();
 // flagshipFrigate.getStatus().then((res) => console.log(res));
