0d871f82c98581bb792622037046f2023bb813e2
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-08 09:20:49 -0400

    add option to log fleet details from api

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ dotenv.config();
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 import { GetMyShips200Response, Ship } from "@spacejunk/airlock";
+import { getMyAgent } from "./agent/agent";
 import { myShips } from "./fleet/ships";
+import { getShipsFromDb } from "./db/getShipsFromDb";
 
 const rl = readline.createInterface({ input, output });
 
@@ -11,7 +13,7 @@ let game = true;
 async function startGame() {
   while (game) {
     const input = await rl.question(
-      "SPACEBOATS\nEnter a command, help, or quit: "
+      "===* SPACEBOATS *===\nEnter a command, help, or quit: "
     );
 
     if (input.toLowerCase() === "quit" || input.toLowerCase() === "q") {
@@ -21,21 +23,29 @@ async function startGame() {
     }
 
     if (input.toLowerCase() === "help" || input.toLowerCase() === "h") {
-      console.log(`no help yet, trying quitting instead`);
+      console.log(`type f or fleet to see fleet details`);
     }
 
     if (input.toLowerCase() === "fleet" || input.toLowerCase() === "f") {
-      console.log("fleet goes here");
-      const response = await myShips();
-      console.log(response.data);
-      //   ships.data.forEach((ship: Ship) => console.log(ship.symbol));
-      //   console.log(ships);
+      const { data, meta } = await myShips();
 
-      //   ships.forEach((ship: Ship) => {
-      //     console.log(ship.symbol);
+      const shipNames = data.map((ship: Ship) => {
+        const {
+          symbol,
+          registration: { role },
+          nav: { waypointSymbol, status },
+        } = ship;
+        return { symbol, role, status, waypointSymbol };
+      });
+      console.log("=== FLEET ===");
+      console.table(shipNames);
+
+      // TODO - get data from API or database?
+      //   const db = await getShipsFromDb();
+      //   const dbShips = db.map((ship: any) => {
+      //     const { pk, symbol, role, navStatus, waypointSymbol } = ship;
+      //     return { pk, symbol, role, navStatus, waypointSymbol };
       //   });
-      //   const names = ships.map((ship: Ship) => ship.symbol);
-      //   console.log(ships);
     }
   }
 }
