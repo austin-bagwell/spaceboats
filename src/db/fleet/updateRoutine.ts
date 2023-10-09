59853409df5e79b1ae7363ba5bc8ad4da59c6573
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-08 20:19:52 -0400

    script to update ship routine in database

---
+++ src/db/fleet/updateRoutine.ts
@@ -0,0 +1,26 @@
+import { prisma } from "../prismaClient";
+
+export type ShipRoutines = "IDLE" | "MINE" | "EXPLORE" | "PING";
+
+type UpdateRoutineArgs = {
+  shipSymbol: string;
+  routine: ShipRoutines;
+};
+export async function updateRoutine({
+  shipSymbol,
+  routine,
+}: UpdateRoutineArgs) {
+  try {
+    const ship = prisma.ship.update({
+      where: {
+        symbol: shipSymbol,
+      },
+      data: {
+        routine,
+      },
+    });
+    return ship;
+  } catch (err: any) {
+    console.log(err);
+  }
+}
