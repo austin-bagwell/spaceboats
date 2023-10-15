577fe521445f26d3e0ea7c26b443813c87bfed0e
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-15 11:28:36 -0400

    add intervalId, which seems futile

--- src/dev.ts
+++ src/dev.ts
@@ -1,6 +1,25 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { startGame } from "./cli/startGame";
+import { getShipsFromDb } from "./db/fleet/getShipsFromDb";
 
-startGame();
+let callCount = 0;
+const intervalId = setInterval(pinger, 1000);
+
+async function pinger() {
+  await getShipsFromDb()
+    .then((ships) => {
+      console.log(`callCount: ${callCount}`);
+      ships.forEach(async (ship) => {
+        if (ship.routine === "PING") {
+          console.log(`Hello from ${ship.symbol}!`);
+        }
+      });
+    })
+    .catch((err) => console.log(err));
+
+  callCount++;
+  if (callCount > 4) {
+    clearInterval(intervalId);
+  }
+}
