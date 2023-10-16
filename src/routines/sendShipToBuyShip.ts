092e32bdec1e9729c1830ac84e704401ae8e3081
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-15 21:21:43 -0400

    import and use fleet api, set dummy values for cooldown, waypoint

--- src/routines/sendShipToBuyShip.ts
+++ src/routines/sendShipToBuyShip.ts
@@ -1,78 +1,97 @@
 import { addShiptoDb } from "../db/fleet/addShipToDb";
 import { getShipFromDb } from "../db/fleet/getShipsFromDb";
+import { wait } from "../utils/wait";
 import { findWaypointWithOptions } from "./findWaypointWithOptions";
+import { FleetApi, ShipType } from "@spacejunk/airlock";
+import { config } from "../utils/config";
+
+const fleet = new FleetApi(config);
 
 type SendSendToBuyShipProps = {
   waypointSymbol?: string;
   sendShip: string;
-  buyShip: string;
+  shipType: ShipType;
 };
 
 export async function sendSendToBuyShip({
   waypointSymbol,
   sendShip,
-  buyShip,
+  shipType,
 }: SendSendToBuyShipProps) {
   // extract system from waypointSymbol
   //    const system = props.waypointSymbol?.split()
   const system = "X1-CS80";
 
-  const waypoint =
-    waypointSymbol ||
-    findWaypointWithOptions({
-      type: "PLANET",
-      system,
-      traitSymbol: "SHIPYARD",
-    });
+  //   need to rewrite this to findWaypointWithTrait, maybe? or allow for no type
+  const waypoint = waypointSymbol
+    ? waypointSymbol
+    : await findWaypointWithOptions({
+        type: "PLANET",
+        system,
+        traitSymbol: "SHIPYARD",
+      });
+
+  if (!waypoint) {
+    throw new Error(`Could't find a waypoint with a shipyard`);
+  }
 
   const {
     waypointSymbol: currentLocation,
     navStatus,
     cooldown,
   } = await getShipFromDb({
-    symbol: buyShip,
+    symbol: sendShip,
   });
 
   //   if ship is present at purchase location and has no cooldown buy the damn ship
   if (currentLocation === waypoint) {
     if (cooldown > 0) {
-      await sleep(cooldown);
+      await wait(cooldown);
     }
 
     const purchasedShip = await fleet.purchaseShip({
-      buyer: sendShip,
-      shipType: buyShip,
+      waypointSymbol: waypoint,
+      shipType,
     });
     return purchasedShip;
   }
 
-  //   if ship isn't already at location
-  if (currentLocation != waypoint || cooldown > 0) {
-    // if cooldown, wait for cooldown
+  //   if ship is not at a either the designated waypoint or the closest with a shipyard
+  if (currentLocation != waypoint) {
     if (cooldown > 0) {
-      await sleep(cooldown);
+      await wait(cooldown);
     }
-    // if docked per db model, orbit ship
+
     if (navStatus === "DOCKED") {
-      await fleet.orbitShip(buyShip);
+      await fleet.orbitShip(sendShip);
     }
 
     // wait until the ship has travelled to it's desination
     // may want to set the cd on the db model instead or in addition to?
-    const { cooldown } = await fleet.navShip({
-      shipSymbol: buyShip,
-      goTo: waypointSymbol,
+    const closestWaypoint = waypoint[0].toString();
+    const {
+      data: {
+        nav: {
+          route: { departureTime, arrival },
+        },
+      },
+    } = await fleet.navigateShip(sendShip, {
+      waypointSymbol: closestWaypoint,
     });
-    await sleep(cooldown);
+    // TODO how to extract arrive tile
+    // const navCooldown = parseTime(arrival - departureTime)
+    const navCooldown = 120;
+    await wait(navCooldown);
 
     // once buying ship arrives, buy the new ship
     const purchasedShip = await fleet.purchaseShip({
-      buyer: sendShip,
-      shipType: buyShip,
+      waypointSymbol: closestWaypoint,
+      shipType,
     });
 
     // be sure to store the newly acquired ship
-    addShiptoDb(purchasedShip);
+    // but response from purchaseShip is different than plain ole ship I think
+    // addShiptoDb(purchasedShip);
 
     // useful to have ship as return value?
     return purchasedShip;
