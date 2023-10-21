394bc319fc14f9684dcc286cbc47deefd9fcf2f6
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-21 14:06:41 -0400

    v1 automine - no selling, minimal db model updating

--- src/routines/mining/automine.ts
+++ src/routines/mining/automine.ts
@@ -1,17 +1,68 @@
-import { getShipsFromDb } from "../../db/getShipsFromDb";
+import { prisma } from "../../db/prismaClient";
+import { getShipFromDb } from "../../db/fleet/getShipsFromDb";
+import { FleetApi } from "@spacejunk/airlock";
+import { config } from "../../utils/config";
+import { wait } from "../../utils/wait";
 
-// const ships = await getShipsFromDb();
+const fleet = new FleetApi(config);
 
 type AutomineProps = {
   shipSymbol: string;
-  waypoint: string;
+  asteroidWaypoint: string;
   contract?: any;
 };
 
-async function automine({ shipSymbol, waypoint }: AutomineProps) {
-  // DB
-  // check ship location, navigate to given waypoint if needed
-  // -- if navigating, set cooldown on ship db object?
+export async function automine({
+  shipSymbol,
+  asteroidWaypoint,
+}: AutomineProps) {
+  const { waypointSymbol, navStatus, cargoCurrent, cargoCapacity } =
+    await getShipFromDb({
+      symbol: shipSymbol,
+    });
+
+  // FIXME - need to make sure db model is being updated anytime a ship is navigated. or when literally anything happens I guess
+  // if ship isn't where it needs to be, send it there
+  // just throw an error for now
+  if (waypointSymbol !== asteroidWaypoint) {
+    throw new Error(
+      `You need to navigate ${shipSymbol} to an asteroid belt first`
+    );
+  }
+
+  // pretty sure you have to be in orbit to extract
+  if (navStatus !== "IN_ORBIT") {
+    await fleet.orbitShip(shipSymbol);
+    // db.ship.update(shipSymbol, { navStatus: "IN_ORBIT" })
+  }
+
+  let capacityPercentage = Math.round((cargoCurrent / cargoCapacity) * 100);
+  while (capacityPercentage < 80) {
+    const {
+      data: {
+        cargo: { units },
+        extraction,
+        cooldown,
+      },
+    } = await fleet.extractResources(shipSymbol);
+    console.log(`${shipSymbol} extracted: ${extraction}`);
+
+    capacityPercentage = Math.round((units / cargoCapacity) * 100);
+    prisma.ship.update({
+      where: {
+        symbol: shipSymbol,
+      },
+      data: {
+        cargoCurrent: units,
+      },
+    });
+
+    await wait(cooldown.remainingSeconds);
+  }
+
+  console.log(
+    `${shipSymbol} did the damn automine thing. Cargo hold is mosly full!`
+  );
 }
 
 /*
