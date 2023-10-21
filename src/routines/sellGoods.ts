888e7a2cf0ddb4cef58453a11adbd1e3236e069b
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-21 14:56:44 -0400

    add some sale options

---
+++ src/routines/sellGoods.ts
@@ -0,0 +1,67 @@
+import { prisma } from "../db/prismaClient";
+import {
+  FleetApi,
+  MarketTransaction,
+  SellCargo201ResponseData,
+} from "@spacejunk/airlock";
+import { config } from "../utils/config";
+import { SellCargoRequest } from "@spacejunk/airlock";
+import { AssertionError } from "assert";
+
+const fleet = new FleetApi(config);
+
+// TODO
+// put MarketTransaction into db
+async function sellGood(
+  shipSymbol: string,
+  cargo: SellCargoRequest
+): Promise<SellCargo201ResponseData> {
+  const { data } = await fleet.sellCargo(shipSymbol, cargo);
+
+  if (!data) {
+    throw new Error(`${shipSymbol} could sell cargo ${cargo}`);
+  }
+
+  await prisma.ship.update({
+    where: {
+      symbol: shipSymbol,
+    },
+    data: {
+      cargoCurrent: data.cargo.units,
+    },
+  });
+
+  return data;
+}
+
+async function sellAllGoods(shipSymbol: string): Promise<MarketTransaction[]> {
+  const { data } = await fleet.getMyShipCargo(shipSymbol);
+
+  const saleRecord: MarketTransaction[] = [];
+  for (let item of data.inventory) {
+    const { symbol, units } = item;
+
+    try {
+      const { data } = await fleet.sellCargo(shipSymbol, { symbol, units });
+      saleRecord.push(data.transaction);
+    } catch (err: any) {
+      console.log(err);
+    }
+  }
+
+  //   TODO - this is probably bad?
+  await prisma.ship.update({
+    where: {
+      symbol: shipSymbol,
+    },
+    data: {
+      cargoCurrent: 0,
+    },
+  });
+
+  //   TODO - add sales records to db
+
+  return saleRecord;
+}
+
+export { sellGood, sellAllGoods };
